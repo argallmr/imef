c16ec00169fe5929ea125d387df49ffb9f4b0e9b
briannaisola <briannaisola@gmail.com>
2022-10-26 18:11:44 -0400

    Fixed unit continuity and dimensions for efield

--- particle-tracing/boris-tracing/bfields.py
+++ particle-tracing/boris-tracing/bfields.py
@@ -1,25 +1,29 @@
 # imports
 import numpy as np
 
+
 def B_dipole(r):
     """
     Compute Earth's dipole magnetic field at given cartesian coordinates.
 
     Args:
-        r (list): array of cartesian coordinates (x, y, z) in units [RE]
+        r (list): array of cartesian coordinates (x, y, z) in units [m]
 
     Returns:
-        float: value of dipole field in units [T]
+        float: value of dipole field in units [T] or [kg*s^-2*A^-1]
     """
-    
-    phi = np.radians(11.7)                                  # tilt of magnetic axis [rad]
-    mu = -7.94e22*np.array([.0, np.sin(phi), np.cos(phi)])  # magnetic moment of earth [A*m^2]
-    M0 = 1.0e-7                                             # mu_0 [N*A−2] /4*PI 
-    RE = 6371000                                            # equatorial radius of Earth [m] 
-    
-    r = r*RE
+
+    # tilt of magnetic axis [rad]
+    phi = np.radians(11.7)
+
+    # magnetic moment of earth [A*m^2]
+    mu = -7.94e22 * np.array([0.0, np.sin(phi), np.cos(phi)])
+
+    # mu_0 [N*A−2] /4*PI
+    M0 = 1.0e-7
+
     rmag = np.sqrt(np.dot(r, r))
-    
-    # calcul;ating dipole field
-    Bdip = M0*(3.*r*np.dot(mu,r)/(rmag**5)-mu/(rmag**3))
+
+    # calculating dipole field
+    Bdip = M0 * (3.0 * r * np.dot(mu, r) / (rmag**5) - mu / (rmag**3))
     return Bdip
--- particle-tracing/boris-tracing/efields.py
+++ particle-tracing/boris-tracing/efields.py
@@ -1,37 +1,43 @@
 import numpy as np
 
+
 def crt_to_sph(x, y, z):
-    r = np.sqrt(x**2 + y**2 + z**2) # radial distance
-    theta = np.arccos(z/r)          # polar angle
-    phi = np.arctan2(y,x)           # azimuthal angle
+    r = np.sqrt(x**2 + y**2 + z**2)  # radial distance
+    theta = np.arccos(z / r)  # polar angle
+    phi = np.arctan2(y, x)  # azimuthal angle
 
     return r, theta, phi
 
-def convection_field(kp):
+
+def convection_field_A0(kp):
     """
-    Compute uniform convection electric field strength in equatorial plane in [kV] with given kp index.
+    Compute uniform convection electric field strength in equatorial plane in [kV/m^2] with given kp index.
+    Based on Maynard and Chen 1975 (doi:10.1029/JA080i007p01009).
 
     Args:
         kp (float): kp index
 
     Returns:
-        float: convection electric field
+        float: A0; convection electric field strength in [mV/m^2]
     """
-    # equatorial radius of earth [km] 
-    RE = 6371
-     
-    # uniform convection electric field strength in equatorial plane [kV]
-    E0 = 0.045/((1-(0.159*kp) + (0.0093*kp**2))**3*(RE**2.))
-    
-    return E0
+    # equatorial radius of earth [m]
+    RE = 6371000
+
+    # uniform convection electric field strength in equatorial plane [kV/m^2]
+    A0 = 0.045 / ((1 - (0.159 * kp) + (0.0093 * kp**2)) ** 3 * (RE**2.0))
+
+    # convert to [mV/m^2]
+    A0 = A0 * 1e6
+
+    return A0
 
 
-def vs_efield(coords, gs, kp, sph = False):
+def vs_efield(coords, gs, kp, sph=False):
     """
     Compute Volland-Stern electric field in [mV/m]
 
     Args:
-        coords (list):          array of coordinates to compute efield in [x,y,z] or [r, theta, phi]; see condition.
+        coords (list):          array of coordinates to compute efield in (x,y,z) or (r, theta, phi), with units in [m]; see 'sph' condition.
         gs (floay):             shielding constant
         kp (float):             kp index
         sph (bool, optional):   Deterimes if input coordinates are in cartesian (False) or spherical (True); Defaults to False.
@@ -39,27 +45,30 @@ def vs_efield(coords, gs, kp, sph = False):
     Returns:
         list: volland-stern field in 3 dimensions.
     """
-    
+
     if sph == False:
         # convert cartesian to spherical coordinates
         rgeo, theta, phi = crt_to_sph(coords[0], coords[1], coords[2])
 
     else:
         # unpack coordinates
-        rgeo  = coords[0]
+        rgeo = coords[0]
         theta = coords[1]
-        phi   = coords[2]
+        phi = coords[2]
 
-    
-    # uniform convection electric field strength in equatorial plane [kV]
-    E0 = convection_field(kp)
+    # uniform convection electric field strength in equatorial plane [mV/m^2]
+    A0 = convection_field_A0(kp)
 
-    # VS efield [kV/km]
-    EC0 = E0*gs*(rgeo**(gs-1))*(np.sin(phi))                        # radial componenet
-    EC1 = 0.                                                        # polar component
-    EC2 = E0*(rgeo**(gs-1))*(1/(np.sin(theta)))*(np.cos(phi))       # azimuthal component
+    # VS efield [kV/m]
 
-    # set to array and convert to [mV/m]
-    EC = np.array([EC0, EC1, EC2])*1e3
-    
+    # radial componenet
+    EC0 = A0 * gs * (rgeo ** (gs - 1)) * (np.sin(phi))
+    # polar component
+    EC1 = 0.0
+    # azimuthal component
+    EC2 = A0 * (rgeo ** (gs - 1)) * (1 / (np.sin(theta))) * (np.cos(phi))
+
+    # set to array
+    EC = np.array([EC0, EC1, EC2])
+
     return EC
