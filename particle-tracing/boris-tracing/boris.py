7d6cfc29cf6acbc2ced2a2ed951d9501d5b7677a
briannaisola <briannaisola@gmail.com>
2022-12-09 15:49:24 -0500

    Boris independent funcction, updated plottools

---
+++ particle-tracing/boris-tracing/boris.py
@@ -0,0 +1,196 @@
+import numpy as np
+import math
+from bfields import *
+from efields import *
+from plottools import *
+
+
+def mag(vec):
+
+    magnitude = np.sqrt(np.dot(vec, vec))
+    return magnitude
+
+
+def crt_to_sph(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)  # radial distance
+    theta = np.arccos(z / r)  # polar angle
+    phi = np.arctan2(y, x)  # azimuthal angle
+
+    return r, theta, phi
+
+
+def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
+    """
+    Boris particle pusher tracing code.
+
+    Args:
+        tf (int): total time in units [s]
+        dt (int): timestep incrementation
+        r0 (ndarray): initial position array in units [m]
+        v0 (ndarray): initial velcoity array in units [m/s]
+        m (float): particle mass in units [kg]
+        q (float): particle charge in units [C]
+        gs (float): shielding constant
+        kp (float): kp index
+        tdir (bool, optional): specifies forward tracing ['fw'] or backward tracing ['bw'].
+                                Defaults to 'fw.'
+
+    Returns:
+        tdat (ndarray): time data in units [s]
+        vdat (ndarray): velocity data in units [m/s]
+        rdat (ndarray): position data in units [m]
+        emag (ndarray): magnitude of total E-field in units [mV/m]
+    """
+    # calculate stepsize (dt) to be no bigger than half the gyroperiod
+    gyroperiod = 1 / ((abs(q) * mag(B_dipole(r0))) / m)
+    dt = round(0.5 * gyroperiod, 2)
+    steps = int(tf / dt)
+
+    tdat = np.zeros(steps - 1)
+    rdat = np.zeros((steps, 3))
+    vdat = np.zeros((steps, 3))
+    emag = np.zeros(steps)
+
+    # set initial conditions
+
+    rdat[0] = r0  # [m]
+    vdat[0] = v0  # [m/s]
+
+    # forward vs. backward tracing
+    if tdir == "fw":
+        n = 1.0
+    else:
+        n = -1.0
+
+    cross = False
+
+    for i in range(0, steps - 1):
+
+        r = rdat[i]
+        v = vdat[i]
+
+        # compute B-field [T]
+        B0 = B_dipole(r)
+        # B0 = np.array([0.,0.,0.])
+
+        # compute convection E-field [mV/m]
+        EC = vs_efield(r, gs, kp)
+
+        # compute corotation E-field [mV/m]
+        ER = corotation_efield(r)
+
+        # compute total E-field and covert to [V/m]
+        # E0 = np.add(EC, ER) * 1e-3
+        E0 = np.array([0.0, 0.0, 0.0])
+
+        c0 = (dt * q * B0) / (2 * m)
+
+        # push step 1 - update velocity with half electrostatic contribution
+        v1 = v + (n * (q * E0 * dt) / (2 * m))
+
+        # push step 2 - rotated via the magnetic field contribution
+        ax = v1 + (n * np.cross(v1, c0))
+        bx = (2 * c0) / (1 + (n * c0**2))
+        v2 = v1 + (n * np.cross(ax, bx))
+
+        # push step 3 - updated again with another half of the electrostatic push [m/s]
+        vnew = v2 + (n * (q * E0 * dt) / (2 * m))
+
+        # update position [m]
+        rnew = r + (n * (vnew * dt))
+
+        # append to data arrays
+        tdat[i] = i * dt if n == 1.0 else tf - i * dt  # time [s]
+        vdat[i + 1] = vnew  # velcoity [m/s]
+        rdat[i + 1] = rnew  # position [m]
+        emag[i] = mag(E0 / 1e-3)  # magnitude of total E-field [mV/m]
+
+        # stop when particle leaves RE = 10 from Earth
+        RE = 6371000
+        rmag = np.sqrt(np.dot(rnew, rnew))
+
+        if rmag >= 10 * RE:
+            if cross == False:
+                tdrift = (i * dt) / 3600
+                tdrift1 = (tf - (i * dt)) / 3600
+            else:
+                rdat[i + 1] = np.nan
+            cross = True
+
+    return tdat, tdrift, vdat, rdat, emag
+
+
+def boris_backward(tf, dt, r0, v0, m, q, gs, kp):
+    """
+    Boris particle pusher backward tracing code.
+
+    Args:
+        tf (int): total time in units [s]
+        dt (int): timestep incrementation
+        r0 (ndarray): initial position array in units [m]
+        v0 (ndarray): initial velcoity array in units [m/s]
+        m (float): particle mass in units [kg]
+        q (float): particle charge in units [C]
+        gs (float): shielding constant
+        kp (float): kp index
+
+    Returns:
+        tdat (ndarray): time data in units [s]
+        vdat (ndarray): velocity data in units [m/s]
+        rdat (ndarray): position data in units [m]
+        emag (ndarray): magnitude of total E-field in units [mV/m]
+    """
+
+    steps = int(tf / dt)
+
+    tdat = np.zeros(steps - 1)
+    rdat = np.zeros((steps, 3))
+    vdat = np.zeros((steps, 3))
+    emag = np.zeros(steps)
+
+    # set initial conditions
+
+    rdat[0] = r0  # [m]
+    vdat[0] = v0  # [m/s]
+
+    for i in range(0, steps - 1):
+
+        r = rdat[i]
+        v = vdat[i]
+
+        # compute B-field [T]
+        B0 = B_dipole(r)
+        # B0 = np.array([0.,0.,0.])
+
+        # compute convection E-field [mV/m]
+        EC = vs_efield(r, gs, kp)
+
+        # compute corotation E-field [mV/m]
+        ER = corotation_efield(r)
+
+        # compute total E-field and covert to [V/m]
+        E0 = np.add(EC, ER) * 1e-3
+
+        c0 = (dt * q * B0) / (2 * m)
+
+        # push step 1 - update velocity with half electrostatic contribution
+        v1 = v - (q * E0 * dt) / (2 * m)
+
+        # push step 2 - rotated via the magnetic field contribution
+        ax = v1 - np.cross(v1, c0)
+        bx = (2 * c0) / (1 - c0**2)  ##
+        v2 = v1 - np.cross(ax, bx)
+
+        # push step 3 - updated again with another half of the electrostatic push [m/s]
+        vnew = v2 - (q * E0 * dt) / (2 * m)
+
+        # update position [m]
+        rnew = r - vnew * dt
+
+        # append to data arrays
+        tdat[i] = tf - i * dt  # time [s]
+        vdat[i + 1] = vnew  # velcoity [m/s]
+        rdat[i + 1] = rnew  # position [m]
+        emag[i] = mag(E0 / 1e-3)  # magnitude of total E-field [mV/m]
+
+    return tdat, vdat, rdat, emag
--- particle-tracing/boris-tracing/plottools.py
+++ particle-tracing/boris-tracing/plottools.py
@@ -1,10 +1,42 @@
 import numpy as np
 import math
+from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.patches import Circle
 from matplotlib.patches import Wedge
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+from boris import *
+
+# colorscales for color consistency
+seq_standard = "YlGnBu"
+div_standard = "RdGy"
+# pcolor = 'GnBu'
+
+# UNH logo hexcode
+nh_blue = "#233E8A"
+white = ["#FFFFFF"]
+
+# UNH-themed sequential colors
+nh_blues = ["#152451", "#233E8A", "#3B62CE", "#7C97DE", "#BDCBEF", "#DFE5F7", "#EFF2FB"]
+nh_grays = ["#333333", "#575757", "#858585", "#ADADAD", "#D6D6D6", "#EBEBEB", "#F5F5F5"]
+nh_blues_r = nh_blues[::-1]
+nh_grays_r = nh_grays[::-1]
+
+# UNH-themed divergent colors
+nh_cscale = nh_blues + white + nh_grays_r
+nh_cscale_r = nh_cscale[::-1]
+
+nh_cmap_div = LinearSegmentedColormap.from_list("cdiv", nh_cscale, N=500)
+nh_cmap_div_r = LinearSegmentedColormap.from_list("cdiv_r", nh_cscale_r, N=500)
+nh_cmap_seq = LinearSegmentedColormap.from_list("cseq", nh_blues, N=500)
+nh_cmap_seq_r = LinearSegmentedColormap.from_list("cseq_r", nh_blues_r, N=500)
+
+
+def save_figure(floc, fname):
+    plt.savefig(floc + fname + ".pdf", bbox_inches="tight", dpi=400, transparent=True)
 
 
 def draw_earth(ax, rad=1, angle=0, ecolor="w", polar=False, style=False, **kwargs):
@@ -161,3 +193,288 @@ def add_offscale_RE(ax, lmax, offset=0.1, half=False, **kwargs):
     r_ax.tick_params(axis="x", length=8, which="major", direction="inout", **kwargs)
     r_ax.tick_params(axis="x", length=5, which="minor", direction="inout", **kwargs)
     r_ax.set_xlabel("[$R_E$]")
+
+
+def center_axis(ax):
+    """
+    Set axis at origin.
+    """
+    # move left y-axis and bottim x-axis to origin
+    ax.spines["left"].set_position("center")
+    ax.spines["bottom"].set_position("center")
+
+    # remove extra axis
+    ax.spines["right"].set_color("none")
+    ax.spines["top"].set_color("none")
+
+    # select axis tick locations
+    ax.xaxis.set_ticks_position("bottom")
+    ax.yaxis.set_ticks_position("left")
+
+    # minor ticks
+    ax.minorticks_on()
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    ax.yaxis.set_minor_locator(MultipleLocator(1))
+
+    # tick styling
+    for axi in ["x", "y"]:
+        ax.tick_params(axis=axi, length=6, which="major", direction="inout")
+        ax.tick_params(axis=axi, length=4, which="minor", direction="inout")
+
+    # endcaps on axis (source: Stack Overflow question 33737736)
+    ax.plot(
+        (1),
+        (0),
+        marker="|",
+        ms=11,
+        color="k",
+        transform=ax.get_yaxis_transform(),
+        clip_on=False,
+    )
+    ax.plot(
+        (0),
+        (1),
+        marker="_",
+        ms=11,
+        color="k",
+        transform=ax.get_xaxis_transform(),
+        clip_on=False,
+    )
+    ax.plot(
+        (0),
+        (0),
+        marker="|",
+        ms=11,
+        color="k",
+        transform=ax.get_yaxis_transform(),
+        clip_on=False,
+    )
+    ax.plot(
+        (0),
+        (0),
+        marker="_",
+        ms=11,
+        color="k",
+        transform=ax.get_xaxis_transform(),
+        clip_on=False,
+    )
+
+
+def smallcoords(ax, dim, hx="x", vx="y", hc="k", vc="k"):
+
+    # horizontal
+    ax.plot([-dim, -dim + 2], [dim - 3, dim - 3], color=hc, lw=1)
+    ax.annotate(
+        hx,
+        xy=(-dim, dim - 3),
+        xycoords="data",
+        xytext=(-dim + 3, dim - 3),
+        textcoords="data",
+        horizontalalignment="right",
+        verticalalignment="center",
+        color=hc,
+    )
+
+    # verical
+    ax.plot([-dim, -dim], [dim - 3, dim - 1], color=vc, lw=1)
+    ax.annotate(
+        vx,
+        xy=(-dim, -dim + 2),
+        xycoords="data",
+        xytext=(-dim, dim),
+        textcoords="data",
+        horizontalalignment="center",
+        verticalalignment="top",
+        color=vc,
+    )
+
+
+def tracingplot_2d(rdat, ax=None, axcenter=True, savefig=True, floc="../bin/"):
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 3)
+        fig.tight_layout(pad=0.5)
+
+    fig.set_figheight(16)
+    fig.set_figwidth(16)
+
+    # radius of earth [m]
+    RE = 6371000
+
+    # scale coordinates in units [RE]
+    rscale = rdat / RE
+
+    ranges = []
+    ranges.append(math.ceil(max(abs(rscale[:, 0]))))
+    ranges.append(math.ceil(min(abs(rscale[:, 0]))))
+    ranges.append(math.ceil(max(abs(rscale[:, 1]))))
+    ranges.append(math.ceil(min(abs(rscale[:, 1]))))
+    ranges.append(math.ceil(max(abs(rscale[:, 2]))))
+    ranges.append(math.ceil(min(abs(rscale[:, 2]))))
+
+    axrng = max(ranges)
+
+    # y-x plot
+    ax[0].plot(rscale[:, 1], rscale[:, 0], "k", linewidth=0.5)
+    ax[0].set_xlabel("$y$ [$R_E$]")
+    ax[0].set_ylabel("$x$ [$R_E$]")
+    ax[0].add_patch(Circle((0, 0), 1.0, color="b", zorder=10))
+
+    # fix axis
+    ax[0].set_xlim([-axrng, axrng])
+    ax[0].set_ylim([-axrng, axrng])
+    ax[0].set_aspect("equal")
+
+    # z-x plol
+    ax[1].plot(rscale[:, 2], rscale[:, 0], "k", linewidth=0.5)
+    ax[1].set_xlabel("$z$ [$R_E$]")
+    ax[1].set_ylabel("$x$ [$R_E$]")
+    ax[1].add_patch(Circle((0, 0), 1.0, color="b", zorder=10))
+
+    # fix axis
+    ax[1].set_xlim([-axrng, axrng])
+    ax[1].set_ylim([-axrng, axrng])
+    ax[1].set_aspect("equal")
+
+    # z-y plot
+    ax[2].plot(rscale[:, 2], rscale[:, 1], "k", linewidth=0.5)
+    ax[2].set_xlabel("$z$ [$R_E$]")
+    ax[2].set_ylabel("$y$ [$R_E$]")
+    ax[2].add_patch(Circle((0, 0), 1.0, color="b", zorder=10))
+
+    # fix axis
+    ax[2].set_xlim([-axrng, axrng])
+    ax[2].set_ylim([-axrng, axrng])
+    ax[2].set_aspect("equal")
+
+    draw_earth(ax[0], ecolor="w", zorder=10)
+    draw_earth(ax[1], ecolor="w", zorder=10)
+    draw_earth(ax[2], ecolor="w", zorder=10)
+
+    # ax.plot(rscale[0][0],rscale[0][1],rscale[0][2],'r',marker='*', markersize=10)
+
+    # plot starting points
+    ax[0].plot(rscale[0][1], rscale[0][0], "r", marker="*", markersize=11, zorder=9)
+    ax[1].plot(rscale[0][2], rscale[0][0], "r", marker="*", markersize=11, zorder=9)
+    ax[2].plot(rscale[0][2], rscale[0][1], "r", marker="*", markersize=11, zorder=9)
+
+    """
+        ax[0].plot(
+        rscale[0][1],
+        rscale[0][0],
+        "y",
+        marker="*",
+        markersize=13,
+        markeredgecolor="k",
+        markeredgewidth=0.25,
+        zorder=9,
+    )
+    
+    """
+
+    datmax = 10
+    ticks = np.arange(-datmax, datmax + 1, 2)
+
+    ax[0].set_xticks(ticks, [abs(i) for i in ticks])
+    ax[0].set_yticks(ticks, [abs(i) for i in ticks])
+    ax[1].set_xticks(ticks, [abs(i) for i in ticks])
+    ax[1].set_yticks(ticks, [abs(i) for i in ticks])
+    ax[2].set_xticks(ticks, [abs(i) for i in ticks])
+    ax[2].set_yticks(ticks, [abs(i) for i in ticks])
+
+    if axcenter == True:
+        # y-x
+        center_axis(ax[0])
+        ax[0].set_xlabel(" ")
+        ax[0].set_ylabel(" ")
+        smallcoords(ax[0], 10, hx="y", vx="x", hc="lime", vc="r")
+
+        # z-x
+        center_axis(ax[1])
+        ax[1].set_xlabel(" ")
+        ax[1].set_ylabel(" ")
+        smallcoords(ax[1], 10, hx="z", vx="x", hc="b", vc="r")
+
+        # z-y
+        center_axis(ax[2])
+        ax[2].set_xlabel(" ")
+        ax[2].set_ylabel(" ")
+        smallcoords(ax[2], 10, hx="z", vx="y", hc="b", vc="lime")
+
+        # get timestamp
+        now = datetime.now()
+        timestamp = now.strftime("%m_%d_%Y_%I_%M_%S_%p")
+
+        # save figure
+        if savefig == True:
+            fname = "TRACING_PLOT_2D_" + "d_" + timestamp
+            save_figure(floc, fname)
+
+
+def tracingplot_3d(rdat, ax=None, savefig=True, floc="../bin/"):
+
+    if ax is None:
+        fig = plt.figure(figsize=(8, 8))
+        ax = plt.axes(projection="3d")
+
+    # radius of earth [m]
+    RE = 6371000
+
+    # scale coordinates in units [RE]
+    rscale = rdat / RE
+
+    # plot dipole field data
+    ax.plot3D(rscale[:, 0], rscale[:, 1], rscale[:, 2], "k", linewidth=0.5)
+
+    # plot starting point
+    ax.plot(
+        rscale[0][0],
+        rscale[0][1],
+        rscale[0][2],
+        "r",
+        marker="*",
+        markersize=10,
+    )
+
+    # plot 3D earth
+    draw_earth3D(ax, ecolor=nh_blue, rad=1)
+
+    # graph correction, keeps origin (0,0,0) at center
+    ranges = []
+    ranges.append(math.ceil(max(abs(rscale[:, 0]))))
+    ranges.append(math.ceil(min(abs(rscale[:, 0]))))
+    ranges.append(math.ceil(max(abs(rscale[:, 1]))))
+    ranges.append(math.ceil(min(abs(rscale[:, 1]))))
+    ranges.append(math.ceil(max(abs(rscale[:, 2]))))
+    ranges.append(math.ceil(min(abs(rscale[:, 2]))))
+
+    axrng = max(ranges)
+
+    ax.set_xlim([-axrng, axrng])
+    ax.set_ylim([-axrng, axrng])
+    ax.set_zlim([-axrng, axrng])
+
+    # plot attributes
+    ax.set_box_aspect([1, 1, 1])
+    ax.set_xlabel("$x$ [$R_E$]")
+    ax.set_ylabel("$y$ [$R_E$]")
+    ax.set_zlabel("$z$ [$R_E$]")
+
+    # Turn off tick labels
+    ax.set_zticklabels([])
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+
+    # color labels
+    ax.xaxis.label.set_color("r")
+    ax.yaxis.label.set_color("lime")
+    ax.zaxis.label.set_color("b")
+
+    # get timestamp
+    now = datetime.now()
+    timestamp = now.strftime("%m_%d_%Y_%I_%M_%S_%p")
+
+    # save figure
+    if savefig == True:
+        fname = "TRACING_PLOT_3D_" + "d_" + timestamp
+        save_figure(floc, fname)
