906b1ca0712d67106bbca17097f0ffde4061a297
Matthew R. Argall <matthew.argall@unh.edu>
2023-06-06 14:23:42 -0400

    Resolved units issue between boris module and notbooks   - boris- new keywords to set the time step, how frequently data is saved, and outer boundary   - boris- position must be in units of RE; velocity is converted to RE/s when updating position   - notebooks- Dipole-0 and Plots-Dipole-Tracing2D3D now agree   - notebooks- Rev_dist_plots works with above changes

--- particle-tracing/boris-tracing/boris.py
+++ particle-tracing/boris-tracing/boris.py
@@ -3,6 +3,7 @@ import math
 from bfields import *
 from efields import *
 from plottools import *
+from tqdm import tqdm
 
 
 # compute vector of numpy array
@@ -10,6 +11,13 @@ def mag(vec):
     magnitude = np.sqrt(np.dot(vec, vec))
     return magnitude
 
+
+def crt2sph(x_ijk):
+    r_ijk = np.zeros_like(x_ijk)
+    r_ijk[:,0] = np.sqrt(np.sum(x_ijk**2, axis=1))
+    r_ijk[:,1] = np.arccos(x_ijk[:,2] / r_ijk[:,0])
+    r_ijk[:,2] = np.arctan2(x_ijk[:,1], x_ijk[:,0])
+    return r_ijk
 
 # convert cartesian to spherical coordinates
 def crt_to_sph(x, y, z):
@@ -25,6 +33,12 @@ def crt_to_sph(x, y, z):
 
     return r, theta, phi
 
+def sph2cart(r_ijk):
+    x_ijk = np.zeros_like(r_ijk)
+    x_ijk[:,0] = r_ijk[:,0] * np.sin(r_ijk[:,1]) * np.cos(r_ijk[:,2])
+    x_ijk[:,1] = r_ijk[:,0] * np.sin(r_ijk[:,1]) * np.sin(r_ijk[:,2])
+    x_ijk[:,2] = r_ijk[:,0] * np.cos(r_ijk[:,1])
+    return x_ijk
 
 # convert spherical to cartesian coordinates
 def sph_to_crt(r, theta, phi):
@@ -41,14 +55,14 @@ def sph_to_crt(r, theta, phi):
     return x, y, z
 
 
-def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
+def boris(tf, r0, v0, m, q, gs, kp, tdir="fw", rmax=10, dn_save=1, dt_wci=0.01):
     """
     Boris particle pusher tracing code.
 
     Args:
         tf (int): total time in units [s]
         dt (int): timestep incrementation
-        r0 (ndarray): initial position array in units [m]
+        r0 (ndarray): initial position array in units [RE]
         v0 (ndarray): initial velcoity array in units [m/s]
         m (float): particle mass in units [kg]
         q (float): particle charge in units [C]
@@ -56,6 +70,7 @@ def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
         kp (float): kp index
         tdir (bool, optional): specifies forward tracing ['fw'] or backward tracing ['bw'].
                                 Defaults to 'fw.'
+        rmax (float, optional): maximum radial distance a particle is allowed to reach
 
     Returns:
         tdat (ndarray): time data in units [s]
@@ -64,18 +79,23 @@ def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
         rdat (ndarray): position data (spherical) in units [m]
         emag (ndarray): magnitude of total E-field in units [mV/m]
     """
+    RE = 6371000
+    
     # calculate stepsize (dt) to be no bigger than half the gyroperiod
-    gyroperiod = 1 / ((abs(q) * mag(B_dipole(r0))) / m)
-    dt = 0.5 * gyroperiod  # round(0.5 * gyroperiod, 2)
+    gyroperiod = (2*np.pi) / ((abs(q) * mag(B_dipole(r0*RE))) / m)
+    dt = dt_wci * gyroperiod  # round(0.5 * gyroperiod, 2)
     # print("tf,dt, r0, q, m", gyroperiod)
     steps = int(tf / dt)
-    print(steps)
+    nout = steps // dn_save
+
+    print('Run time: {0}, Time step: {1}, Steps: {2}'
+          .format(tf, dt, steps))
 
     # old
-    tdat = np.zeros((steps - 1,))
-    rdat = np.zeros((steps, 3)) * np.nan
-    vdat = np.zeros((steps, 3))
-    emag = np.zeros((steps,))  # * np.nan
+    tdat = np.zeros((nout,))
+    rdat = np.zeros((nout, 3)) * np.nan
+    vdat = np.zeros((nout, 3))
+    emag = np.zeros((nout,))  # * np.nan
     # print("tdat =", tdat[1])
 
     # tdat = np.array([np.nan] * steps - 1)
@@ -84,8 +104,12 @@ def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
     # emag = np.array([np.nan] * steps)
 
     # set initial conditions
-    rdat[0] = r0  # [m]
-    vdat[0] = v0  # [m/s]
+    tdat[0] = 0
+    rdat[0] = r0  # [RE]
+    vdat[0] = v0  # [RE/s]
+    isave = 1     # We have already saved the first data point at t=0
+    rnew = r0
+    vnew = v0
 
     # forward vs. backward tracing
     if tdir == "fw":
@@ -93,13 +117,15 @@ def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
     else:
         n = -1.0
 
-    for i in range(0, steps - 1):
+    for i in tqdm(range(0, steps - 1)):
+        # print('{0} of {1}; Saving {2} of {3}'.format(i, steps, int(i // dn_save), nout))
+
         # set current position and velocity (cartesian coords)
-        r = rdat[i]
-        v = vdat[i]
+        r = rnew # rdat[i]
+        v = vnew # vdat[i]
 
         # compute B-field [T]
-        B0 = B_dipole(r, sph=False)
+        B0 = B_dipole(r*RE, sph=False)
         # test function: no B-field
         # B0 = np.array([0.0, 0.0, 0.0])
 
@@ -130,29 +156,38 @@ def boris(tf, r0, v0, m, q, gs, kp, tdir="fw"):
         # push step 3 - updated again with another half of the electrostatic push [m/s]
         vnew = v2 + (n * (q * E0 * dt) / (2 * m))
 
-        # update position [m]
-        rnew = r + (n * (vnew * dt))
+        # update position [RE]
+        rnew = r + (n * (vnew * dt) / RE)
 
-        # append to data arrays
-        tdat[i] = i * dt  # if n == 1.0 else tf - i * dt  # time [s]
-        vdat[i + 1] = vnew  # velcoity [m/s]
-        rdat[i + 1] = rnew  # position [m]
-        emag[i] = mag(E0 / 1e-3)  # magnitude of total E-field [mV/m]
+        # Append to data arrays
+        #   - Iteration i creates data for i+1
+        if ((i+1) % dn_save) == 0:
+            # print('Saving {0} of {1}'.format(i // dn_save, nout))
+            tdat[isave] = (i+1) * dt  # if n == 1.0 else tf - i * dt  # time [s]
+            vdat[isave] = vnew  # velcoity [m/s]
+            rdat[isave] = rnew  # position [RE]
+            emag[isave] = mag(E0 / 1e-3)  # magnitude of total E-field [mV/m]
+            isave += 1
 
         # calculate driftt time when particle leaves L=10
 
         # find positional magnitude
-        RE = 6371000
         rmag = np.sqrt(np.dot(rnew, rnew))
         # """
         # check if particle(s) crossed rmax
-        rmax = 12 * RE
         if rmag >= rmax:
             tdrift = (i + 1) * dt
             break
         else:
             tdrift = np.nan
     # """
+
+    # Trim the data if rmag > rmax
+    if isave < nout:
+        tdat = tdat[:isave]
+        rdat = tdat[:isave,:]
+        vdat = tdat[:isave,:]
+        edat = tdat[:isave,:]
 
     # tdrift = 1.0
 
